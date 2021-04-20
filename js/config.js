cc6f01e0549139b0b9c6e0a78e75958d6bc6661f
Jose G. Cabrera <72307972+Jose-G-Cabrera@users.noreply.github.com>
2021-04-19 21:47:21 -0500

    updating config.js settings

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_Ii6VKzg57', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4oq7bn4tk2c5ir1jm18rd5d44j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
